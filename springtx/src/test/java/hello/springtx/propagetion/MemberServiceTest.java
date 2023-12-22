7a495af00a465008414bb0ad8ba66c5d591fbca5
LeeSPyo <oco141@naver.com>
2023-12-22 15:10:29 +0900

    트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ springtx/src/test/java/hello/springtx/propagetion/MemberServiceTest.java
@@ -0,0 +1,38 @@
+package hello.springtx.propagetion;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import hello.springtx.propagation.LogRepository;
+import hello.springtx.propagation.MemberRepository;
+import hello.springtx.propagation.MemberService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+	@Autowired
+	MemberService memberService;
+	@Autowired
+	MemberRepository memberRepository;
+	@Autowired
+	LogRepository logRepository;
+
+	/**
+	 * MemberService @Transactional:OFF MemberRepository @Transactional:ON
+	 * LogRepository @Transactional:ON
+	 */
+	@Test
+	void outerTxOff_success() {
+		//given
+		String username = "outerTxOff_success";
+		//when
+		memberService.joinV1(username);
+		//then: 모든 데이터가 정상 저장된다.
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isPresent());
+	}
+}
