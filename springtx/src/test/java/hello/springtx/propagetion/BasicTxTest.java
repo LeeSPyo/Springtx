78be4d56f5fa1444b6a47f7e0af68937a98f9913
LeeSPyo <oco141@naver.com>
2023-12-22 13:27:48 +0900

    스프링 트랜잭션 전파4 - 전파 예제

--- springtx/src/test/java/hello/springtx/propagetion/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagetion/BasicTxTest.java
@@ -57,4 +57,18 @@ public class BasicTxTest {
 		log.info("트랜잭션2 커밋");
 		txManager.commit(tx2);
 	}
+
+	@Test
+	void inner_commit() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+		log.info("내부 트랜잭션 시작");
+		TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+		log.info("내부 트랜잭션 커밋");
+		txManager.commit(inner);
+		log.info("외부 트랜잭션 커밋");
+		txManager.commit(outer);
+	}
 }
