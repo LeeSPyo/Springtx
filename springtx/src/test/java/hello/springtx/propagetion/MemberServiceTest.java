30a51862e650f993c629ce1b08e97243b4b04276
LeeSPyo <oco141@naver.com>
2023-12-26 10:29:07 +0900

    트랜잭션 전파 활용6 - 복구 REQUIRED

--- springtx/src/test/java/hello/springtx/propagetion/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagetion/MemberServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import hello.springtx.propagation.LogRepository;
 import hello.springtx.propagation.MemberRepository;
@@ -68,8 +69,7 @@ class MemberServiceTest {
 	}
 
 	/**
-	 * MemberService @Transactional:ON 
-	 * MemberRepository @Transactional:ON
+	 * MemberService @Transactional:ON MemberRepository @Transactional:ON
 	 * LogRepository @Transactional:ON
 	 */
 	@Test
@@ -84,8 +84,7 @@ class MemberServiceTest {
 	}
 
 	/**
-	 * MemberService @Transactional:ON 
-	 * MemberRepository @Transactional:ON
+	 * MemberService @Transactional:ON MemberRepository @Transactional:ON
 	 * LogRepository @Transactional:ON Exception
 	 */
 	@Test
@@ -94,6 +93,21 @@ class MemberServiceTest {
 		String username = "로그예외_outerTxOn_fail";
 		// when
 		assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+		// then: 모든 데이터가 롤백된다.
+		assertTrue(memberRepository.find(username).isEmpty());
+		assertTrue(logRepository.find(username).isEmpty());
+	}
+
+	/**
+	 * MemberService @Transactional:ON MemberRepository @Transactional:ON
+	 * LogRepository @Transactional:ON Exception
+	 */
+	@Test
+	void recoverException_fail() {
+		// given
+		String username = "로그예외_recoverException_fail";
+		// when
+		assertThatThrownBy(() -> memberService.joinV2(username)).isInstanceOf(UnexpectedRollbackException.class);
 		// then: 모든 데이터가 롤백된다.
 		assertTrue(memberRepository.find(username).isEmpty());
 		assertTrue(logRepository.find(username).isEmpty());
